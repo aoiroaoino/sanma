774d058abf5652697722ca3427149d6d0ff05b00
Aoino <aoiro.aoino@gmail.com>
2014-03-15 21:36:34 +0900

    add sample data to List view.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,6 +2,14 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+// import akka.actor._
+import dispatch._
+import scala.io.Source
+
+import play.api.Play.current
+import scala.concurrent.duration.DurationInt
+import play.api.libs.concurrent.Akka
+import play.api.libs.concurrent.Execution.Implicits._
 
 object Application extends Controller {
 
@@ -9,4 +17,18 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
+  def hoge = Action {
+    Ok(views.html.aaa(List("aaa","bbb","ccc")))
+  }
+
+  def registration = Action {
+    Ok(views.html.registration())
+  }
+
+  def list = Action {
+    Ok(views.html.list(List(
+      ("進捗どうですか？", "締め切りという絶対的存在に挑み、そして儚く散っていった者たちの物語"),
+      ("だばだー","だばだーだー"))
+    ))
+  }
 }
